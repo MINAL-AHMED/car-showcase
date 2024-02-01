592a26b9a146eb6fe39c85c6c9e7616d5cf46770
Minal Ahmed <mimalahmed6219@gmail.com>
2024-02-02 00:19:56 +0600

    import analytics form vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
-import './globals.css'
-
-import { NavBar, Footer } from '@/components'
-
+import { Footer, NavBar } from "@/components";
+import { Analytics } from "@vercel/analytics/react";
+import "./globals.css";
 
 export const metadata = {
-  title: 'Car Hub',
-  description: 'Discover the best cars in the world.',
-}
+  title: "Car Hub",
+  description: "Discover the best cars in the world.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className="relative">
-        <NavBar/>
+        <NavBar />
         {children}
-        <Footer/>
-        </body>
+        <Analytics />
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
